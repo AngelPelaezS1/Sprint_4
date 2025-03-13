7d4a76e614055a7958f326b079e9cf9457cf6716
Angel Pelaez <angelpelaezsanz@gmail.com>
2025-03-13 11:22:55 +0100

    Update FruitController.java

--- Nivel1/src/main/java/cat/itacademy/s04/t02/n01/controllers/FruitController.java
+++ Nivel1/src/main/java/cat/itacademy/s04/t02/n01/controllers/FruitController.java
@@ -1,44 +1,54 @@
-package cat.itacademy.s04.t02.n01.controllers;
+package cat.itacademy.s04.t02.n03.controller;
 
-import cat.itacademy.s04.t02.n01.services.FruitService;
+import cat.itacademy.s04.t02.n03.service.FruitService;
+import org.springframework.http.ResponseEntity;
+import cat.itacademy.s04.t02.n03.model.Fruit;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import cat.itacademy.s04.t02.n01.model.Fruit;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/fruit")
 public class FruitController {
-
     private final FruitService fruitService;
 
-    @Autowired
-    public FruitController(FruitService fruitService) {
+    public FruitController(FruitService fruitService){
         this.fruitService = fruitService;
+
     }
 
     @PostMapping("/add")
-    public void addFruit(@RequestBody Fruit fruit){
-        fruitService.add(fruit);
+    public ResponseEntity<Fruit> addFruit(@RequestBody Fruit fruit){
+        Fruit createdFruit = fruitService.add(fruit);
+        return ResponseEntity.ok(createdFruit);
+
     }
 
     @GetMapping("/getAll")
-    public List<Fruit> getAllFruits(){
-        return fruitService.getAll();
+    public ResponseEntity<List<Fruit>> getAllFruits(){
+        List<Fruit> fruits = fruitService.getAll();
+        return ResponseEntity.ok(fruits);
     }
 
     @GetMapping("/getOne/{id}")
-    public Fruit getOneFruit(@PathVariable long id){
-        return fruitService.getOne(id);
-    }
-
-    @PutMapping("/update/{id}")
-    public String updateFruit(@RequestBody Fruit fruit, @PathVariable long id){
-        return fruitService.update(fruit, id);
+    public ResponseEntity<Fruit> getOneFruit(@PathVariable String id){
+        Fruit fruit = fruitService.getOne(id);
+        return ResponseEntity.ok(fruit);
     }
 
     @DeleteMapping("/delete/{id}")
-    public String deleteFruit(@PathVariable long id){
-        return fruitService.delete(id);
+    public ResponseEntity<Void> deleteFruit(@PathVariable String id){
+        boolean fruitDelete = fruitService.delete(id);
+        if (fruitDelete) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Fruit> updateFruit(@PathVariable String id, @RequestBody Fruit fruit){
+        Fruit updateFruit = fruitService.update(fruit, id);
+        return ResponseEntity.ok(updateFruit);
     }
 }
